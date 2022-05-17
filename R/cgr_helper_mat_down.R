19c39988b10b69160fc5a158f7588149b1bd0b7b
Daniel Gomon <83280434+d-gomon@users.noreply.github.com>
2022-05-17 15:44:35 +0200

    cgr_down now works "correctly". Need to check whether val_up works as it's supposed to.

--- R/cgr_helper_mat_down.R
+++ R/cgr_helper_mat_down.R
@@ -174,16 +174,22 @@ cgr_helper_mat_down <- function(data, ctimes, h, coxphmod, cbaseh, ncores, displ
     AT <- sum(lambdamat[matsub, which(ctimes == ctime)])
     #THIS COULD BE SLOW, OTHERWISE ASSIGN TDAT <- subset(data, matsub)
     #Determine amount of failures at ctime.
-    NDT <- length(which(data[matsub, ]$censorid == 1 & data[matsub,]$otime <= ctime))
+    NDT <- length(which(data[matsub, ]$censorid == 1 & data[matsub, ]$otime <= ctime))
+    NDT_current <- length(which(data[matsub, ]$censorid == 1 & data[matsub, ]$otime == ctime))
     #Determine MLE of theta
     thetat <- log(NDT/AT)
+    thetat_down <- log((NDT-NDT_current)/AT)
     if (is.finite(thetat)){
       thetat <- max(min(0, thetat), -abs(maxtheta))
     } else {thetat <- 0}
+    if (is.finite(thetat_down)){
+      thetat_down <- max(min(0, thetat_down), -abs(maxtheta))
+    } else {thetat_down <- 0}
     #Determine value of CGI-CUSUM using only patients with S_i > helperstimes[i]
     CGIvalue <- - thetat * NDT + (exp(thetat)- 1) * AT
+    CGI_down <- - thetat_down * (NDT - NDT_current) + (exp(thetat_down)- 1) * AT
     #Return both the value of CGI and the MLE (to be used later)
-    return(c(CGIvalue, thetat))
+    return(c(CGI_down, CGIvalue, thetat_down, thetat))
   }
 
   #Function to calculate CGR value at one ctime by ways of maximizing over all CGI(t) values.
@@ -199,9 +205,11 @@ cgr_helper_mat_down <- function(data, ctimes, h, coxphmod, cbaseh, ncores, displ
                                      lambdamat = lambdamat, maxtheta = maxtheta))
     #If there are no values to be calculated, return trivial values
     if(length(a) == 0){
-      return(c(0,0,1))
+      #Returns CGI-val_down, CGI-val, thetaval_down, thetaval, index of determined stime,
+      return(c(0,0,0,0,1))
     }else{
       #First row is value of chart, second row associated value of theta
+      #Third row is value of chart without current failures
       #Determine which entry is the smallest (largest CGI value)
       tidmin <- which.min(a[1,])
       #Determine the corresponding value of CGI(t)
@@ -221,18 +229,23 @@ cgr_helper_mat_down <- function(data, ctimes, h, coxphmod, cbaseh, ncores, displ
     a <- sapply(helperstimes[which(helperstimes <= y)],
                 function(x) maxoverk(helperstime = x,  ctime = y, ctimes = ctimes, data = data,
                                      lambdamat = lambdamat, maxtheta = maxtheta))
-    #First row is value of chart, second row associated value of theta
-    #Determine which entry is the largest (largest CGI value)
-    tidmin <- which.min(a[1,])
-    #Determine the corresponding value of CGI(t)
-    atemp <- a[,tidmin]
-    if(abs(atemp[1]) >= abs(h)){
-      hcheck <<- TRUE
-      stopind <<- TRUE
-      stopctime <<- match(y, ctimes)
+    if(length(a) == 0){
+      #Returns CGI-val, CGI-val_current, thetaval, index of determined stime,
+      return(c(0,0,0,0,1))
+    }else{
+      #First row is value of chart, second row associated value of theta
+      #Determine which entry is the largest (largest CGI value)
+      tidmin <- which.min(a[1,])
+      #Determine the corresponding value of CGI(t)
+      atemp <- a[,tidmin]
+      if(abs(atemp[1]) >= abs(h)){
+        hcheck <<- TRUE
+        stopind <<- TRUE
+        stopctime <<- match(y, ctimes)
+      }
+      #Returns c(chartval, thetaval) at maximum of CGI(t)
+      return(c(atemp, tidmin))
     }
-    #Returns c(chartval, thetaval) at maximum of CGI(t)
-    return(c(atemp, tidmin))
   }
 
   #hcheck used to check whether value of CGR has surpassed control limit
@@ -251,27 +264,42 @@ cgr_helper_mat_down <- function(data, ctimes, h, coxphmod, cbaseh, ncores, displ
       stopCluster(cl)
       cl <- NULL
     }
-    fin <- pbsapply(ctimes, function(x){ if(isFALSE(hcheck)){ maxoverj_h(x, h = h)} else{return(c(0,0,1))}}, cl = cl)
+    fin <- pbsapply(ctimes, function(x){ if(isFALSE(hcheck)){ maxoverj_h(x, h = h)} else{return(c(0,0,0,0,1))}}, cl = cl)
   } else{
     fin <- pbsapply(ctimes, maxoverj, cl = cl)
     if(ncores > 1){
       stopCluster(cl)
     }
   }
-  Gt <- as.data.frame(t(fin))
+  print(stopctime)
+  print(stopctime2)
+  Gt <- t(fin)
   if(!is.na(stopctime)){
     Gt <- Gt[1:stopctime,]
   }
-  Gt[,2] <- exp(Gt[,2])
-  Gt[,3] <- helperstimes[Gt[,3]]
+  Gt[,3] <- exp(Gt[,3])
+  Gt[,4] <- exp(Gt[,4])
+  Gt[,5] <- helperstimes[Gt[,5]]
   if(!is.na(stopctime)){
     Gt <- cbind(ctimes[1:stopctime], Gt)
   } else{
     Gt <- cbind(ctimes, Gt)
   }
-  Gt <- rbind(c(min(data$entrytime), 0, 1, 0),Gt)
-  colnames(Gt) <- c("time", "value", "exp_theta_t", "S_nu")
+  Gt <- unname(Gt)
+  #Initiate final matrix
+  Gt_final <- matrix(c(min(ctimes, min(data$entrytime)), 0, 1, 0), ncol = 4)
+  for(l in 1:nrow(Gt)){
+    if(Gt[l, 2] == Gt[l,3]){
+      Gt_final <- rbind(Gt_final, Gt[l, c(1, 2, 4, 6)])
+    } else{
+      Gt_final <- rbind(Gt_final, Gt[l, c(1, 2, 4, 6)])
+      Gt_final <- rbind(Gt_final, Gt[l, c(1, 3, 5, 6)])
+    }
+  }
+  Gt_final <- as.data.frame(Gt_final)
+
+  colnames(Gt_final) <- c("time", "value", "exp_theta_t", "S_nu")
 
   #return list of relevant values
-  return(Gt)
+  return(Gt_final)
 }
