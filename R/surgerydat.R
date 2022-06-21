205eddf74b15f428fd1de30ad639ab654f4e4e1b
Daniel Gomon <83280434+d-gomon@users.noreply.github.com>
2022-06-21 10:26:37 +0200

    Fixed surgerydat description

--- R/surgerydat.R
+++ R/surgerydat.R
@@ -5,14 +5,14 @@
 #' with patient arrivals in the first 400 days after the start of the study. \cr
 #' Patient survival times were determined using a risk-adjusted Cox proportional hazards model
 #' with coefficients age = 0.003, BMI = 0.02 and sexmale = 0.2 and exponential baseline hazard rate
-#' \eqn{h_0(t, \lambda = 0.01) e^\mu}{h_0(t, \lambda = 0.01) exp(\mu)}.
-#' Some hospitals have an increased failure rate:
+#' \eqn{h_0(t, \lambda = 0.01) e^\mu}{h_0(t, \lambda = 0.01) exp(\theta)}.
+#' The increase in hazard rate is sampled from a normal distribution for all hospitals:
 #' \itemize{
-#' \item Hospitals 1-15: \eqn{\mu \sim N(log(0.5), sd = 0.25)}{\mu ~ N(log(0.5), sd = 0.25)}
-#' \item Hospitals 16-30: \eqn{\mu \sim N(log(1), sd = 0.25)}{\mu ~ N(log(1), sd = 0.25)}
-#' \item Hospitals 31-45: \eqn{\mu \sim N(log(2), sd = 0.25)}{\mu ~ N(log(2), sd = 0.25)}
-#' } which means that the hazard rate at hospitals 31-45 is twice higher than exponential(\eqn{\lambda})
-#' and twice smaller at hospitals 1-15. \cr
+#' \item \eqn{\theta \sim N(log(1), sd = 0.4)}{\theta ~ N(log(1), sd = 0.4)}
+#' } This means that the average failure rate of hospitals in the data set
+#' should be baseline (\eqn{\theta = 0}{\theta = 0}), with some hospitals
+#' experiencing higher and lower failure rates. True failure rate can be found
+#' in the column \code{exptheta}. \cr
 #' The arrival rate \eqn{\psi}{\psi} of patients at a hospital differs. The arrival rates are:
 #' \itemize{
 #' \item Hospitals 1-5 & 16-20: 0.5 patients per day (small hospitals)
@@ -27,7 +27,7 @@
 #'   \item{entrytime}{Time of entry of patient into study (numeric)}
 #'   \item{survtime}{Time from entry until failure of patient (numeric)}
 #'   \item{censorid}{Censoring indicator (0 - right censored, 1 - observed) (integer)}
-#'   \item{hosp_num}{Hospital number at which patient received treatment (integer)}
+#'   \item{unit}{Hospital number at which patient received treatment (integer)}
 #'   \item{exptheta}{True excess hazard used for generating patient survival (numeric)}
 #'   \item{psival}{Poisson arrival rate at hospital which the patient was at (numeric)}
 #'   \item{age}{Age of the patient (numeric)}
